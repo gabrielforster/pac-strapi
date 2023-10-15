6bc8764e00f08714b8f89602919f7e0510604688
Gabriel Rocha <rochafrgabriel@gmail.com>
2023-10-14 21:41:37 -0300

    feat: create proxy api

---
+++ api/index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+require('dotenv').config();
+
+const API_TOKEN = process.env.API_TOKEN;
+
+const proxy = httpProxy.createProxyServer({});
+
+const server = http.createServer((req, res) => {
+  const target = {
+    target: 'http://18.231.106.52:1337',
+  };
+
+  proxy.on('proxyReq', (proxyReq, req, res, options) => {
+    proxyReq.setHeader('Authorization', `Bearer ${API_TOKEN}`);
+  });
+
+  proxy.web(req, res, target);
+});
+
+const port = 3000; 
+server.listen(port, () => {
+  console.log(`Proxy server is running on port ${port}`);
+});
